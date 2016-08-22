74731137b58e47619cc835c64ba76f5cca3ea19e
Paul Hauner <paul@paulhauner.com>
2016-08-22 16:49:22 +1000

    Update settings

--- local_settings.py
+++ local_settings.py
@@ -2,10 +2,10 @@ import os
 
 ARCANIST_LOCATION = "/usr/local/bin/arc"
 
-BASE_DOMAIN = "devphab.paulh.io:8081"
+BASE_DOMAIN = "127.0.0.1:8082"
 PHAB_API_ADDRESS = "http://" + BASE_DOMAIN
 
-PHAB_API_TOKEN = "api-265t7eybohvvkfxidvsywwjieshr"
+PHAB_API_TOKEN = "api-slyo55xyw7dxd2zkggwujuiaoolz"
 
 PHAB_DB_HOST = "database"
 PHAB_DB_USER = "admin"
@@ -13,4 +13,4 @@ PHAB_DB_PASSWORD = "lkj6alkj56a3njhauijlz"
 
 
 # super users get added to all projects
-PHAB_SUPER_USER_PHIDS = ['PHID-USER-oluorlo4dyo6p7erkro7',]
+PHAB_SUPER_USER_PHIDS = ['PHID-USER-74vxekmlathzwtr5vqru',]
